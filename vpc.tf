ac5d5ba31554c0e3b4207db9798a756176ab6a76
virtualsrini <virtual.srini@gmail.com>
2024-04-18 13:12:11 +0530

    sync

--- vpc.tf
+++ vpc.tf
@@ -1,3 +1,5 @@
+# Internet Access VPC
+
 resource "aws_vpc" "code_test" {
     cidr_block = "10.2.0.0/16"
     instance_tenancy = "default"
@@ -6,4 +8,46 @@ resource "aws_vpc" "code_test" {
     tags = {
       Name = "main"
     } 
+}
+
+# Subnet Details
+
+resource "aws_subnet" "main_public-1" {
+  vpc_id = "aws_vpc.code_testd.id"
+  cidr_block = "10.2.1.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "main_public-1"
+  }  
+}
+
+resource "aws_subnet" "main_public-2" {
+  vpc_id = "aws_vpc.code_test.id"
+  cidr_block = "10.2.2.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = "true"
+  tags = {
+    Name = "main_public-2"
+  }  
+}
+
+resource "aws_subnet" "main_private-1" {
+  vpc_id = "aws_vpc.code_test.id"
+  cidr_block = "10.2.3.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = "false"
+  tags = {
+    Name = "main_private-1"
+  }  
+}
+
+resource "aws_subnet" "main_private-2" {
+  vpc_id = "aws_vpc.code_test.id"  
+  cidr_block = "10.2.4.0/24"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = "fal
+  tags = {
+    Name = "main_private-2"
+  }
 }
