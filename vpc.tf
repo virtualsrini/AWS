f131f32b581eb68bf3f50581dfbead0f6c203d32
virtualsrini <virtual.srini@gmail.com>
2024-04-18 12:29:35 +0530

    submit

--- vpc.tf
+++ vpc.tf
@@ -1,1 +1,9 @@
-test config
+resource "aws_vpc" "code_test" {
+    cidr_block = "10.2.0.0/16"
+    instance_tenancy = "default"
+    enable_dns_hostnames = "true"
+    enable_dns_support = "true"
+    tags = {
+      Name = "main"
+    } 
+}
