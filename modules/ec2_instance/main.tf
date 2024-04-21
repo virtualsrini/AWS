78b51c2679eb91e7382d6d24f127204747e8bbd3
virtualsrini <virtual.srini@gmail.com>
2024-04-21 17:45:01 +0530

    sync

---
+++ modules/ec2_instance/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+
+}
+
+variable "instance_type" {
+  description = "This is instance type, like nano, medium, small., etc"
+}
+
+variable "ami" {
+  description = "This is AMI for the instance"
+}
+
+resource "aws_instance" "example" {
+  ami = var.ami
+  instance_type = var.instance_type
+}
